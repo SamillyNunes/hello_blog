f7e5ea6b43a2c259a1455a8c5459e22ef8bf4083
Samilly <samillynunes19@gmail.com>
2025-02-14 18:00:09 -0300

    feat: navigating to Post page using dynamic routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Menu from "./components/Menu";
 import Footer from "components/Footer";
 import DefaultPage from "components/DefaultPage";
+import Post from "pages/Post";
 
 function AppRoutes() {
   return (
@@ -17,6 +18,7 @@ function AppRoutes() {
         <Route path="/" element={<DefaultPage />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
         <Route path="*" element={<div>Pagina nao encontrada!</div>} />
       </Routes>
