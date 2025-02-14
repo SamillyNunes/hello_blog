b1048008a6159326620ffc4b7d6015ea9ffe8148
Samilly <samillynunes19@gmail.com>
2025-02-14 18:59:48 -0300

    feat: Post page created with react markdown lib and some styles

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
+import postsJson from "json/posts.json";
+import ModelPost from "components/ModelPost";
+import Markdown from "react-markdown";
 
-export default function Post(){
-    const params = useParams();
+import './Post.css';
 
-    console.log(params);
-    
-    return (
-        <h1>Post {params.id } </h1>
-    );
+export default function Post() {
+  const params = useParams();
+
+  const post = postsJson.find((p) => p.id === Number(params.id));
+
+  return (
+    <ModelPost
+      coverImg={`/assets/posts/${post.id}/capa.png`}
+      title={post.titulo}
+    >
+        {/* Como o texto do post eh atualizado de maneira dinamica, nao da para usar
+        css em modulos ou derivados, precisa ser o tradicional */}
+      <div className="post-markdown-container">
+        <Markdown children={post.texto} />
+      </div>
+    </ModelPost>
+  );
 }
