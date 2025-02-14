273a73aa63519e75016c8535dd5c0abb3f9ab982
Samilly <samillynunes19@gmail.com>
2025-02-14 17:54:19 -0300

    chore: renamed post component to post card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,13 @@
 import posts from "json/posts.json";
 import styles from "./Home.module.css";
-import Post from "components/Post";
+import PostCard from "components/PostCard";
 
 export default function Home() {
   return (
     <ul className={styles.posts}>
       {posts.map((p) => (
         <li key={p.id}>
-          <Post post={p} />
+          <PostCard post={p} />
         </li>
       ))}
     </ul>
