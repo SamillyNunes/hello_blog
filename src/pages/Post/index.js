0747018d5880152d929e6a604bc889b5a8d22013
Samilly <samillynunes19@gmail.com>
2025-02-17 09:27:29 -0300

    feat: presenting not found page when post doesnt exist

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,29 +1,40 @@
-import { useParams } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import postsJson from "json/posts.json";
 import ModelPost from "components/ModelPost";
 import Markdown from "react-markdown";
 
-import './Post.css';
+import "./Post.css";
+import NotFound from "pages/NotFound";
+import DefaultPage from "components/DefaultPage";
 
 export default function Post() {
   const params = useParams();
 
   const post = postsJson.find((p) => p.id === Number(params.id));
 
-  if(!post){
-    return <h1>Post não encontrado</h1>;
+  if (!post) {
+    return <NotFound />;
   }
 
   return (
-    <ModelPost
-      coverImg={`/assets/posts/${post.id}/capa.png`}
-      title={post.titulo}
-    >
-        {/* Como o texto do post eh atualizado de maneira dinamica, nao da para usar
-        css em modulos ou derivados, precisa ser o tradicional */}
-      <div className="post-markdown-container">
-        <Markdown children={post.texto} />
-      </div>
-    </ModelPost>
+    <Routes>
+      <Route path="*" element={<DefaultPage />}>
+        <Route
+          index
+          element={
+            <ModelPost
+              coverImg={`/assets/posts/${post.id}/capa.png`}
+              title={post.titulo}
+            >
+              {/* Como o texto do post eh atualizado de maneira dinamica, nao da para usar
+              css em modulos ou derivados, precisa ser o tradicional */}
+              <div className="post-markdown-container">
+                <Markdown children={post.texto} />
+              </div>
+            </ModelPost>
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -19,8 +19,8 @@ function AppRoutes() {
         <Route path="/" element={<DefaultPage />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="posts/:id" element={<Post />} />
         </Route>
+        <Route path="posts/:id" element={<Post />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
 
